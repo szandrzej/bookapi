9397c21a94836f8c178199953a0ff3ad419504bc
Andrzej Szmajnta <andrzejdaniel.szm@gmail.com>
2015-07-18 23:35:45 +0200

    Clear logger

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ var validator = require('./helpers/requestValidator');
 
 var app = express();
 
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(cors());
-//app.use(logger('dev'));
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -59,6 +59,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.send();
 });
-
 
 module.exports = app;
